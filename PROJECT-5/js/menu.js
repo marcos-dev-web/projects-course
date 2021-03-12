a40e419da9059420c37a34ffc284ef349f59a70a
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-03-12 11:48:13 -0300

    finished header and banner P5

---
+++ PROJECT-5/js/menu.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector("#menu_button");
+const menuView = document.querySelector('#menu_view');
+const body = document.body;
+
+let open = true;
+
+const toggle = () => {
+    menuView.classList.toggle('show', open);
+    if (open) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.removeAttribute('style');
+    }
+    open = !open;
+}
+
+menuButton.addEventListener('click', toggle);
